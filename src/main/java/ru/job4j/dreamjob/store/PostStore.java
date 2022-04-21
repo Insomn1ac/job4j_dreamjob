0904f4d86dbff18b22dbd2bdaca7aef8e2d7cd76
Insomn1ac <emelyanoff94@gmail.com>
2022-04-21 12:21:32 +0300

    Thymeleaf, Циклы. [#504841]

---
+++ src/main/java/ru/job4j/dreamjob/store/PostStore.java
@@ -0,0 +1,32 @@
+package ru.job4j.dreamjob.store;
+
+import ru.job4j.dreamjob.model.Post;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class PostStore {
+    private static final PostStore INST = new PostStore();
+    private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
+
+    private PostStore() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        posts.put(1, new Post(1, "Junior Java Job",
+                "Job for junior Java developer", LocalDateTime.now().minusMinutes(1).format(formatter)));
+        posts.put(2, new Post(2, "Middle Java Job",
+                "Job for middle Java developer", LocalDateTime.now().minusHours(3).format(formatter)));
+        posts.put(3, new Post(3, "Senior Java Job",
+                "Job for senior Java developer", LocalDateTime.now().minusDays(1).format(formatter)));
+    }
+
+    public static PostStore instOf() {
+        return INST;
+    }
+
+    public Collection<Post> findAll() {
+        return posts.values();
+    }
+}
