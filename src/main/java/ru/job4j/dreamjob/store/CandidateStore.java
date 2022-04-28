c04e45c18b840cb7c8b28f7af5931bc5b2799028
Insomn1ac <emelyanoff94@gmail.com>
2022-04-28 10:47:35 +0300

    Создание и редактирование кандидатов. [#504858 #294304]

--- src/main/java/ru/job4j/dreamjob/store/CandidateStore.java
+++ src/main/java/ru/job4j/dreamjob/store/CandidateStore.java
@@ -7,11 +7,13 @@ import java.time.format.DateTimeFormatter;
 import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class CandidateStore {
     private static final CandidateStore INST = new CandidateStore();
     private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
     private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    private final AtomicInteger id = new AtomicInteger(3);
 
     private CandidateStore() {
         candidates.put(1, new Candidate(1, "Ivan Ivanov",
@@ -31,6 +33,7 @@ public class CandidateStore {
     }
 
     public void add(Candidate candidate) {
+        candidate.setId(id.incrementAndGet());
         candidate.setCreated(LocalDateTime.now().format(formatter));
         candidates.putIfAbsent(candidate.getId(), candidate);
     }
