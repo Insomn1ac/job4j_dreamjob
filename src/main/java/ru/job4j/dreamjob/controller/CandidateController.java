9eeb19b1188000832d64e161d0246cf3959fedf5
Insomn1ac <emelyanoff94@gmail.com>
2022-04-29 19:18:17 +0300

    Формы. Загрузка файла на сервер. [#504855]

--- src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
+++ src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
@@ -1,12 +1,20 @@
 package ru.job4j.dreamjob.controller;
 
 import net.jcip.annotations.ThreadSafe;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import ru.job4j.dreamjob.model.Candidate;
 import ru.job4j.dreamjob.service.CandidateService;
 import ru.job4j.dreamjob.service.CityService;
+
+import java.io.IOException;
 
 @Controller
 @ThreadSafe
@@ -32,7 +40,10 @@ public class CandidateController {
     }
 
     @PostMapping("/createCandidate")
-    public String createCandidate(@ModelAttribute Candidate candidate, @RequestParam("city.id") int id) {
+    public String createCandidate(@ModelAttribute Candidate candidate,
+                                  @RequestParam("city.id") int id,
+                                  @RequestParam("file") MultipartFile file) throws IOException {
+        candidate.setPhoto(file.getBytes());
         candidate.setCity(cityService.findById(id));
         service.add(candidate);
         return "redirect:/candidates";
@@ -50,5 +61,15 @@ public class CandidateController {
         candidate.setCity(cityService.findById(id));
         service.update(candidate);
         return "redirect:/candidates";
+    }
+
+    @GetMapping("/photoCandidate/{candidateId}")
+    public ResponseEntity<Resource> download(@PathVariable("candidateId") Integer candidateId) {
+        Candidate candidate = service.findById(candidateId);
+        return ResponseEntity.ok()
+                .headers(new HttpHeaders())
+                .contentLength(candidate.getPhoto().length)
+                .contentType(MediaType.parseMediaType("application/octet-stream"))
+                .body(new ByteArrayResource(candidate.getPhoto()));
     }
 }
--- src/main/java/ru/job4j/dreamjob/model/Candidate.java
+++ src/main/java/ru/job4j/dreamjob/model/Candidate.java
@@ -10,6 +10,7 @@ public class Candidate implements Serializable {
     private String created;
     private boolean visible;
     private City city;
+    private byte[] photo;
 
     public Candidate() {
 
@@ -74,6 +75,14 @@ public class Candidate implements Serializable {
 
     public void setCity(City city) {
         this.city = city;
+    }
+
+    public byte[] getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(byte[] photo) {
+        this.photo = photo;
     }
 
     public boolean isVisible() {
